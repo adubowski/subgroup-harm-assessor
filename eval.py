e8f75eff979a12c29770fcb3a0a4eb3c22b7fe66
Adam Dubowski <adubowski@deeploy.ml>
2024-02-26 10:52:01 +0100

    Fix CLI arg for train split

--- eval.py
+++ eval.py
@@ -1,10 +1,8 @@
 import logging
 import time
-from typing import List
 import shap
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
 from sklearn.tree import DecisionTreeClassifier
